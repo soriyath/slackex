95dfb49b91a159ac7c0e7e3b40ba9c763754e552
soriyath <soriyath@straessle.io>
2019-10-29 17:26:56 +0100

    Upgrade dependencies

--- mix.exs
+++ mix.exs
@@ -2,22 +2,23 @@ defmodule Slackex.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :slackex,
-     version: "0.0.1",
-     elixir: "~> 1.2",
-     description: description,
-     package: package,
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     deps: deps]
+    [
+      app: :slackex,
+      version: "0.0.1",
+      elixir: "~> 1.2",
+      description: description,
+      package: package,
+      build_embedded: Mix.env() == :prod,
+      start_permanent: Mix.env() == :prod,
+      deps: deps
+    ]
   end
 
   # Configuration for the OTP application
   #
   # Type "mix help compile.app" for more information
   def application do
-    [applications: [:logger, :httpoison, :exjsx],
-     mod: {Slackex, []}]
+    [applications: [:logger, :httpoison, :exjsx], mod: {Slackex, []}]
   end
 
   # Dependencies can be Hex packages:
@@ -31,10 +32,10 @@ defmodule Slackex.Mixfile do
   # Type "mix help deps" for more examples and options
   defp deps do
     [
-      {:httpoison, "~> 0.8"},
-      {:exjsx, "~> 3.2.0", app: false},
-      {:earmark, "~> 0.1", only: :dev},
-      {:ex_doc, "~> 0.11", only: :dev}
+      {:httpoison, "~> 1.6"},
+      {:exjsx, "~> 4.0", app: false},
+      {:earmark, "~> 1.4.2", only: :dev},
+      {:ex_doc, "~> 0.21", only: :dev}
     ]
   end
 
@@ -46,9 +47,11 @@ defmodule Slackex.Mixfile do
   end
 
   defp package do
-    [ files: [ "lib", "mix.exs", "README.md", "LICENSE" ],
-      maintainers: [ "David Stump" ],
-      licenses: [ "MIT" ],
-      links: %{ "GitHub" => "https://github.com/davidstump/slackex" } ]
+    [
+      files: ["lib", "mix.exs", "README.md", "LICENSE"],
+      maintainers: ["David Stump"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/davidstump/slackex"}
+    ]
   end
 end
